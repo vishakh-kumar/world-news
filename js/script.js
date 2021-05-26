a165e56fc0e5b3d33d321948f60cd3de71f2c533
Vishakh Aswani Kumar <vishakh@Vishakhs-MacBook-Pro.local>
2021-05-26 16:29:35 -0500

    wip: updated it to incorporate updates from .html

--- js/script.js
+++ js/script.js
@@ -6,22 +6,24 @@
 // cached element references
 let $main = $("main");
 
+
 //funtions
 // we need newsArray.content
 function handleGetData(event) {
     event.preventDefault();
     $main.empty();
-    let searchText = $("input#inputBtn").val()
+    let searchText = $("input#inputBtn").val();
+   
 
     $.ajax(`https://gnews.io/api/v4/search?q=${searchText}&token=c4c8a4cebef621ab5eafecf6b7a504ea`)
         .then(function (data) {
                 eachNews = data.articles;
 
-                for (i = 0; i < 6; i++) {
+                for (i = 0; i < 10; i++) {
                     let newsArray = eachNews[i];
                     let content = newsArray.content;
-
                     $main.append(`
+                
         <article>
         <h3>${newsArray.title}</h3>
         <p>${newsArray.description}</p>
